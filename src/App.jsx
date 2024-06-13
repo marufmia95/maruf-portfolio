0a34806d7de2b928ca74ec466a587a29972feb0b
marufmia95 <md.marufmia4768@gmail.com>
2024-06-13 22:33:58 +0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Skills from './components/Skills'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <>
     <Navbar/>
     <Hero/>
+    <Skills/>
     </>
   )
 }
