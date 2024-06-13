b7c5bff80b2c48b130cf91ed14efc44d8e9e627c
marufmia95 <md.marufmia4768@gmail.com>
2024-06-13 17:13:36 +0600

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import heroimage from '../assets/profilepic7.jpeg'
+import {TypeAnimation} from 'react-type-animation'
 
 const Hero = () => {
     return (
-        <div>
-            <h1>hero</h1>
-        </div>
+        <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] h-[70vh] mx-auto py-8 bg-black'>
+                <div className='w-[200px]  h-auto mx-auto'>
+                    <div className='w-[300px]  h-auto lg:w-[400px]'>
+                        <img className='rounded-full' src={heroimage} alt=" hero image" />
+                    </div>
+                </div>
+
+            </div>
     );
 };
 
