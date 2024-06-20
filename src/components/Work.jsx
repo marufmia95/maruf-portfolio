c498bb35d2a8b4a4ff7a9133ca5a0263e0eccd45
marufmia95 <md.marufmia4768@gmail.com>
2024-06-20 09:12:35 +0600

    Update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,7 +2,7 @@
 import proj1 from '../assets/proj1.png'
 import proj2 from '../assets/proj2.png'
 import proj3 from '../assets/proj3.png'
-import proj4 from '../assets/proj4.png'
+import proj4 from '../assets/proj4.jpg'
 import proj5 from '../assets/proj5.png'
 import proj6 from '../assets/proj6.png'
 
